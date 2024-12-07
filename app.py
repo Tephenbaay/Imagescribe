62bfbb5cbf7a79f67a203a73550ae7440394592a
Tephenbaay <76113565+Tephenbaay@users.noreply.github.com>
2024-12-07 19:48:25 +0800

    Updated port binding to use the PORT environment variable

--- app.py
+++ app.py
@@ -383,4 +383,5 @@ def upload():
 # Remove the db.create_all() here and instead, handle migrations with Flask-Migrate
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from env, default to 5000
+    app.run(host="0.0.0.0", port=port)
